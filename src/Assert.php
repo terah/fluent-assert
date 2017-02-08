d46f84e814ec39e0465208d5dab83d072af1fcbe
uqcullen <uqcullen@uq.edu.au>
2017-02-08 17:28:58 +1000

    Adding validator class

--- src/Assert.php
+++ src/Assert.php
@@ -139,12 +139,34 @@ class Assert
     }
 
     /**
-     * @param $value
+     * @param        $value
+     * @param string $name
+     * @param int    $code
+     * @param string $error
+     * @param string $level
      * @return Assert
      */
-    public static function that($value)
+    public static function that($value, $name='', $code=0, $error='', $level=Assert::WARNING)
     {
-        return new Assert($value);
+        $assert = new static($value);
+        if ( $name )
+        {
+            $assert->name($name);
+        }
+        if ( $code )
+        {
+            $assert->code($code);
+        }
+        if ( $error )
+        {
+            $assert->error($error);
+        }
+        if ( $level )
+        {
+            $assert->level($level);
+        }
+
+        return $assert;
     }
 
     /**
